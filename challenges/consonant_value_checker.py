e652fd45eead149abff1e1ac731f8ed5bc6cf545
FridaGathima <frida.gathima@student.moringaschool.com>
2023-08-21 10:40:45 +0300

    challenge 3

--- challenges/consonant_value_checker.py
+++ challenges/consonant_value_checker.py
@@ -1,35 +1,23 @@
 def consonant_value_checker(word):
-    vowels = "AaEeiIOoUu"
-    str2 = ""
-
+    consonants = "bcdfghjklmnpqrstvwxyz" 
+    current_value = 0
+    max_value = 0
+   
     for letter in word:
-        if letter not in vowels:
-            str2 = str2+letter
-            return(str2)
+        if letter in consonants:
+            current_value += ((ord(letter) - ord("a")) + 1)
+            max_value = max(max_value, current_value)
+         
+        else: 
+            current_value = 0
 
-print(consonant_value_checker("hello"))
-
-
-    
-    # if word != vowels:
-    #     r
-    
-    # for i in word:
-    #     if i == vowels:
-    # print (int(word - vowels))
-            
-# print(consonant_value_checker("hello"))
-# def albabet_counter(letter):
-#     if letter != "a" and letter != "b":
-#         return(letter)
-#     else:
-#         return(letter - "a" - "b")
+    return max_value
 
 
-# alphabet = ["a", "b", "c", "d"]
-# alphabet_numbers = [letter for letter in alphabet if letter != "a" and letter != "b"]
-# print(alphabet_numbers)
-    
-# print(albabet_counter("adele"))
-# # print(consonant_value_checker())
+input_word = input("Enter a String: ")
+
+result = consonant_value_checker(input_word)
+
+print(result)
+
  
